ef5b50d9ea7dabcb0370b72faeb24feca6d9c988
Robin Hossain <robinhossain5261@gmail.com>
2022-10-25 00:39:10 +0600

    Navbar Complete

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,31 +1,47 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../../../images/learn-logo.jpg';
 
 const Header = () => {
     return (
         <div>
-            <div className="navbar bg-base-100">
+            <div className="navbar bg-gray-100 lg:px-24" >
                 <div className="navbar-start">
                     <div className="dropdown">
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
                         <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><Link>Item 1</Link></li>
-                            <li><Link>Item 3</Link></li>
+                            <li><Link>Home</Link></li>
+                            <li><Link>Courses</Link></li>
+                            <li><Link>FAQ</Link></li>
+                            <li><Link>Blog</Link></li>
+                            <li><Link>About</Link></li>
+
+                            <div className='lg:hidden'>
+                                <Link className="btn btn-outline btn-info mr-2">Login</Link>
+                                <Link className="btn btn-outline btn-info">Register</Link>
+                            </div>
                         </ul>
                     </div>
-                    <Link className="btn btn-ghost normal-case text-xl">Learn-Code</Link>
+                    <div className='flex items-center	'>
+                        <img className='w-16 h-16 rounded-xl' src={logo} alt="" />
+                        <Link className="btn btn-ghost normal-case text-3xl ">Learn <span className='text-info'>-Code</span></Link>
+                    </div>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal p-0">
-                        <li><Link>Item 1</Link></li>
+                        <li><Link>Home</Link></li>
+                        <li><Link>Courses</Link></li>
+                        <li><Link>FAQ</Link></li>
+                        <li><Link>Blog</Link></li>
+                        <li><Link>About</Link></li>
 
-                        <li><Link>Item 3</Link></li>
                     </ul>
                 </div>
-                <div className="navbar-end">
-                    <Link className="btn">Get started</Link>
+                <div className="navbar-end lg:flex hidden">
+                    <Link className="btn btn-outline btn-info mr-2">Login</Link>
+                    <Link className="btn btn-outline btn-info">Register</Link>
                 </div>
             </div>
         </div>
