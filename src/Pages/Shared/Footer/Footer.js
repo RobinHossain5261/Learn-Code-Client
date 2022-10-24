92bb101c685cd045dda49eed5ec1fd9cf94918d8
Robin Hossain <robinhossain5261@gmail.com>
2022-10-24 23:31:19 +0600

    first commit

---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <p>Copyright @2022 by Robin-hossain</p>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyC1k2V2YJI6zGvEVA18A6Gt12YbTKYK4uI",
+    authDomain: "learn-code-b6e05.firebaseapp.com",
+    projectId: "learn-code-b6e05",
+    storageBucket: "learn-code-b6e05.appspot.com",
+    messagingSenderId: "499655806870",
+    appId: "1:499655806870:web:70594d0d6a9ada637f57b7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
